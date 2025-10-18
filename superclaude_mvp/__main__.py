9908b619b0c121b66a519d2990e2512aff0cffea
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-18 17:41:30 +0300

    Initial MVP: Simple CLI framework with command system

---
+++ superclaude_mvp/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+SuperClaude MVP
+Entry point when running as: python -m superclaude_mvp
+"""
+
+import sys
+from superclaude_mvp.cli import main
+
+if __name__ == "__main__":
+    sys.exit(main())
